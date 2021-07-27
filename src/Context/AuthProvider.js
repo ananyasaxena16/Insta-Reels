8b407de6eebfec3be799c4b8babb12b5c81f91c1
Ananya Saxena <80524036+ananyasaxena16@users.noreply.github.com>
2021-07-27 22:52:10 +0530

    Add files via upload

--- src/Context/AuthProvider.js
+++ src/Context/AuthProvider.js
@@ -1,46 +1,41 @@
-import React,{useState, useEffect, useContext} from 'react'
-import {auth} from '../firebase';
+import React,{useState,useContext,useEffect} from 'react'
+import {auth} from '../firebase'
 export const AuthContext = React.createContext();
 function AuthProvider({children}) {
-        const [currentUser, setCurrentUser] = useState();
-        const [loading, setLoading] = useState(true);
-
-        function signup(email, password){
-            return auth.createUserWithEmailAndPassword(email, password);
+    const[currentUser,setCurrentUser] =useState();
+    const[loading,setLoading] =useState(true);
+    function signup(email,password)
+    {
+        return auth.createUserWithEmailAndPassword(email,password);
+    }
+    function login(email,password)
+    {
+        return auth.signInWithEmailAndPassword(email,password);
+    }
+    function logout()
+    {
+        return auth.signOut();
+    }
+    useEffect(()=>{
+        const unsubscribe  = auth.onAuthStateChanged(user=>{
+            setCurrentUser(user);
+            console.log(user);
+            setLoading(false);
+        })
+        return ()=>{
+            unsubscribe();
         }
-        function login(email, password){
-            return auth.signInWithEmailAndPassword(email, password);
-        }
-        function logout(){
-            return auth.signOut();
-        }
-        useEffect(()=>{
-            const unsubscribe = auth.onAuthStateChanged(user=>{ //Componentdidmount
-                setCurrentUser(user);
-                //console.log(user);
-                setLoading(false);
-            })
-            return()=>{ //ComponentWillunmount
-                unsubscribe();
-            }
-        },[])
-
-        const value ={
-            currentUser,
-            login,
-            signup,
-            logout
-        }
-    
-    
-    
+    },[])
+    const value = {
+        currentUser,
+        login,
+        signup,
+        logout
+    }
     return (
-        
-            <AuthContext.Provider value={value}>
-                {!loading&&children}
-            </AuthContext.Provider>
-            
-    
+        <AuthContext.Provider value={value}>
+            {!loading&&children}
+        </AuthContext.Provider>
     )
 }
 
